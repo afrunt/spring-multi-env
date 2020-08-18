56a433b18f91136aa22d56d44319708e14ed92c1
Andrii Frunt <afrunt@bottomline.com>
2020-08-19 01:37:46 +0300

    hello

--- src/main/java/com/afrunt/spring/multienv/ContextBuilder.java
+++ src/main/java/com/afrunt/spring/multienv/ContextBuilder.java
@@ -6,13 +6,13 @@ import org.springframework.core.env.CompositePropertySource;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.env.PropertySource;
-import org.springframework.util.Assert;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static org.springframework.util.Assert.noNullElements;
 import static org.springframework.util.Assert.notNull;
 
 public abstract class ContextBuilder {
@@ -100,17 +100,21 @@ public abstract class ContextBuilder {
         notNull(defaultProfiles, "defaultProfiles cannot be null");
     }
 
+    protected <T extends GenericApplicationContext> T populateEnvironmentState(T ctx) {
+        ctx.getEnvironment().setActiveProfiles(activeProfiles.toArray(new String[]{}));
+        ctx.getEnvironment().setDefaultProfiles(defaultProfiles.toArray(new String[]{}));
+        ctx.getEnvironment().getPropertySources().addLast(propertySource);
+        return ctx;
+    }
+
     public static class AnnotationConfigContextBuilder extends ContextBuilder {
         private List<String> basePackages = new ArrayList<>();
         private List<Class<?>> componentClasses = new ArrayList<>();
 
         @Override
-        public GenericApplicationContext build() {
+        public AnnotationConfigApplicationContext build() {
             validateConfiguration();
-            AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-            ctx.getEnvironment().setActiveProfiles(activeProfiles.toArray(new String[]{}));
-            ctx.getEnvironment().setDefaultProfiles(defaultProfiles.toArray(new String[]{}));
-            ctx.getEnvironment().getPropertySources().addLast(propertySource);
+            AnnotationConfigApplicationContext ctx = populateEnvironmentState(new AnnotationConfigApplicationContext());
 
             if (!basePackages.isEmpty()) {
                 ctx.scan(basePackages.toArray(new String[]{}));
@@ -127,16 +131,17 @@ public abstract class ContextBuilder {
 
         public AnnotationConfigContextBuilder basePackages(String... basePackages) {
             notNull(basePackages, "basePackages cannot be null");
-            Assert.noNullElements(basePackages, "basePackage cannot be null");
+            noNullElements(basePackages, "basePackage cannot be null");
 
             this.basePackages = Arrays.stream(basePackages)
                     .collect(Collectors.toList());
+
             return this;
         }
 
         public AnnotationConfigContextBuilder componentClasses(Class<?>... componentClasses) {
             notNull(componentClasses, "componentClasses cannot be null");
-            Assert.noNullElements(componentClasses, "componentClass cannot be null");
+            noNullElements(componentClasses, "componentClass cannot be null");
 
             this.componentClasses = Arrays.stream(componentClasses)
                     .collect(Collectors.toList());
