1e9d79bc54432f7dcdb1e4fb611067f8e02ac73a
Andrii Frunt <afrunt@bottomline.com>
2020-08-18 21:52:30 +0300

    hello

--- src/test/java/com/afrunt/spring/multienv/ContextBuilderTest.java
+++ src/test/java/com/afrunt/spring/multienv/ContextBuilderTest.java
@@ -4,45 +4,30 @@ import com.afrunt.spring.multienv.ctx.simple.ProductionProfileBean;
 import com.afrunt.spring.multienv.ctx.simple.SimpleBean;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.support.GenericApplicationContext;
 
 import java.util.Map;
-import java.util.function.Consumer;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ContextBuilderTest {
     @Test
     public void testHappyPath() {
-        ContextBuilder packageBuilder = ContextBuilder
-                .annotationConfig("com.afrunt.spring.multienv.ctx.simple")
-                .addMapPropertySource(Map.of("envProp", "envValue"))
-                .activeProfiles("production")
-                .defaultProfiles();
-
-        ContextBuilder classesBuilder = ContextBuilder
-                .annotationConfig(SimpleBean.class, ProductionProfileBean.class)
-                .addMapPropertySource(Map.of("envProp", "envValue"))
-                .activeProfiles("production")
-                .defaultProfiles();
-
-        Consumer<GenericApplicationContext> happyPathConsumer = ctx -> {
-            SimpleBean simpleBean = ctx.getBean(SimpleBean.class);
-            assertNotNull(simpleBean);
-            assertEquals("envValue", simpleBean.getEnvProp());
-            assertNotNull(ctx.getBean(ProductionProfileBean.class));
-        };
-
-        happyPathConsumer.accept(packageBuilder.build());
-        happyPathConsumer.accept(classesBuilder.build());
+        Stream.of(createPackageContextBuilder(), createClassesBuilder())
+                .map(ContextBuilder::build)
+                .forEach(ctx -> {
+                    SimpleBean simpleBean = ctx.getBean(SimpleBean.class);
+                    assertNotNull(simpleBean);
+                    assertEquals("envValue", simpleBean.getEnvProp());
+                    assertNotNull(ctx.getBean(ProductionProfileBean.class));
+                });
     }
 
     @Test
-    public void testPropertySourcePriority(){
-        GenericApplicationContext ctx = ContextBuilder
-                .annotationConfig("com.afrunt.spring.multienv.ctx.simple")
-                .addMapPropertySource(Map.of("envProp", "envValue"))
+    public void testPropertySourcePriority() {
+        GenericApplicationContext ctx = createPackageContextBuilder()
                 .addMapPropertySource(Map.of("envProp", "higherPriorityValue"))
                 .build();
 
@@ -50,7 +35,32 @@ public class ContextBuilderTest {
     }
 
     @Test
-    public void testEmptyPackagesAndClasses(){
+    public void testEmptyPackagesAndClasses() {
         Assertions.assertThrows(IllegalStateException.class, () -> ContextBuilder.annotationConfig().build());
+    }
+
+    @Test
+    public void testWithoutProductionProfile() {
+        assertThrows(NoSuchBeanDefinitionException.class, () -> createPackageContextBuilder()
+                .activeProfiles()
+                .build()
+                .getBean(ProductionProfileBean.class)
+        );
+    }
+
+    private ContextBuilder createPackageContextBuilder() {
+        return ContextBuilder
+                .annotationConfig("com.afrunt.spring.multienv.ctx.simple")
+                .addMapPropertySource(Map.of("envProp", "envValue"))
+                .activeProfiles("production")
+                .defaultProfiles();
+    }
+
+    private ContextBuilder createClassesBuilder() {
+        return ContextBuilder
+                .annotationConfig(SimpleBean.class, ProductionProfileBean.class)
+                .addMapPropertySource(Map.of("envProp", "envValue"))
+                .activeProfiles("production")
+                .defaultProfiles();
     }
 }
